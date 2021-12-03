e9c70ff8f1b023334aa3fb4c0091643a1692235c
Anish <anishsingh1452002@gmail.com>
2021-12-03 11:45:46 +0530

    Completed till tree

---
+++ data_structures/queues/implementation_using_CircularArray.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//array implementation
+//define the size of the queue
+#define maxSize 101
+//static circular array
+// next element = (i+1)%N
+//previous element = (i+N-1)%N
+/*
+    fundamental operations
+        1. push(x) or enqueue(x)
+        2. pop() or dequeue
+        3. peek()
+        4. isEmpty()
+        5. isFull()
+        6. init()
+*/
+struct Queue
+{
+    int front;
+    int rear;
+    int staticQueueArr[maxSize];
+}QueueStatic;
+
+void init()
+{
+   QueueStatic.front = QueueStatic.rear = -1;
+}
+bool isEmpty()
+{
+    if (QueueStatic.front == -1)
+        return true;
+    else
+        return false;
+}
+
+bool isFull()
+{
+    if (QueueStatic.rear >= maxSize)
+        return true;
+    else
+        return false;
+}
+void push(int value)
+{
+    if (isFull())
+    {
+        cout << "Queue is OVERFLOW , no more push operation available. :(" << endl;
+    }
+    else
+    {
+        if(QueueStatic.front == -1)
+            QueueStatic.front=0;
+        
+        QueueStatic.rear++;
+        QueueStatic.staticQueueArr[QueueStatic.rear] = value;
+        
+        cout << "Element pushed!!!" << endl;
+    }
+}
+
+int pop()
+{
+    if (isEmpty() || QueueStatic.front > QueueStatic.rear)
+    {
+        cout << "Queue is UNDERFLOW, push some elements to pop. :)" << endl;
+    }
+    else
+    {
+        int popElement = QueueStatic.staticQueueArr[QueueStatic.front];
+        QueueStatic.front++;
+        cout << "The Element popped: " << popElement << endl;
+
+    }
+}
+int peek()
+{
+    return QueueStatic.staticQueueArr[QueueStatic.front];
+}
+int main()
+{
+    int choice,value;
+    init();
+    while(1)
+    {
+        cout << "0.Exit" << endl;
+        cout << "1.Push" << endl;
+        cout << "2.Pop" << endl;
+        cout << "3.Is Empty?" << endl;
+        cout << "4.Is Full?" << endl;
+        cout << "5.Peek" << endl;
+
+        cout << "Enter you Choice: ";
+        cin >> choice;
+        switch(choice)
+        {
+            case 0: exit(0); break;
+
+            case 1: cout << "Enter the value that needs to be pushed: ";
+            cin >> value;
+            push(value);
+            break;
+
+            case 2: pop();
+            break;
+
+            case 3: if(isEmpty()) cout << "Queue is empty!!!" << endl;
+            else cout << "Queue is not empty!!!" <<endl;
+            break;
+
+            case 4: if(isFull()) cout << "Queue is full!!!" << endl;
+            else cout << "Queue is not full!!!" <<endl;
+            break;
+
+            case 5: cout << "The Top element is " << peek() << endl;
+            break;
+        }
+    }
+    return 0;
+}
--- data_structures/stacks/reversal_of_LL_using_stack.cpp
+++ data_structures/stacks/reversal_of_LL_using_stack.cpp
@@ -14,15 +14,99 @@
 using namespace std;
 
 //For this question I'll be using stack from STL although we can even  define our own stack and perform the reverse LL function 
+struct Node
+{
+    int data;
+    Node *next;
+}*head=NULL , *tail = NULL;
+
+void append(int value)
+{
+    //create a new node
+    Node *temp=new Node();
+    temp->data=value;
+    temp->next=NULL;
+    //if head is NULL, it is an empty list
+    if(head == NULL)
+        head = tail = temp;
+    //Otherwise, find the tail and add the temp
+    else
+    {
+        // tail = head;
+
+        //last node's next address will be NULL.
+        while(tail->next != NULL)
+        {
+            tail = tail->next;
+        }
+
+        //add the temp at the end of the linked list
+        tail->next = temp;
+    }
+
+} 
+void display()
+{
+    //creating a pointer that is pointing towards head and iterating forward
+    Node *ptr = head;
+    while(ptr!=NULL)
+    {
+        cout << ptr->data << " ";
+        ptr = ptr->next;
+    }
+}
+
 
 void reverseLL()
 {
-    
+    stack<Node *> S;
+    if(!head) 
+        return;
+
+
+    //loop for push LL node
+    Node *temp = head;
+
+    while(temp)
+    {
+        S.push(temp);
+        temp = temp->next;
+    }
+
+    //loop for pop LL node
+    temp = S.top(); //point towards the last inserted node
+    head = temp; //make head the last inserted node
+    S.pop(); //pop the last inserted node
+    //check if the LL is empty or not
+    while(!S.empty())
+    {
+        //dry run this to understand
+        temp->next= S.top();
+        S.pop();
+        temp=temp->next;
+    }
+    temp->next=NULL;
 }
 
 
 
 int main()
 {
+    int n;
+    cout << "Enter the size of the Linked List: ";
+    cin >> n;
+    int value,pos;
+    for(int i=0;i<n;i++)
+    {
+        cout << "Enter the number: ";
+        cin >> value;
+        append(value);
+    }
+    cout << "The original linked list: " << endl;
+    display();
+    cout << endl;
+    reverseLL();
+    cout << "The reverse linked list: " << endl;
+    display();
     return 0;
 }
